94edf0baebe363dc9e41049b2991dc86f4be5dcd
Igor Souza <igormcsouza@gmail.com>
2021-02-26 20:05:10 -0300

    Fix skills

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,27 +1,19 @@
 import React from "react";
-import { createStyles, Divider, makeStyles, Theme } from "@material-ui/core";
+import {
+  createStyles,
+  Divider,
+  Grid,
+  Hidden,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
 
 const useStyle = makeStyles((theme: Theme) =>
   createStyles({
     skillBox: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "center",
-      height: "100%",
-    },
-    background: {
-      height: 400,
-      maxWidth: 600,
-      textAlign: "justify",
-      display: "flex",
-      alignItems: "center",
-    },
-    skills: {
-      heitght: 400,
-      maxWidth: 600,
-      textAlign: "justify",
-      display: "flex",
-      alignItems: "center",
+      marginLeft: "auto",
+      marginRight: "auto",
+      margin: "50px 0px",
     },
   })
 );
@@ -30,27 +22,44 @@ const Skill: React.FC<{}> = () => {
   const classes = useStyle();
 
   return (
-    <div className={classes.skillBox}>
-      <div className={classes.background}>
-        Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi.
-        Mauris aliquet nunc non turpis scelerisque, eget. Atirei o pau no gatis,
-        per gatis num morreus. Paisis, filhis, espiritis santis. Posuere libero
-        varius.
-        <br />
-        <br />
-        Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Aenean
-        aliquam molestie leo, vitae iaculis nisl. Per aumento de cachacis, eu
-        reclamis. Leite de capivaris, leite de mula manquis sem cabeça. Manduma
-        pindureta quium dia nois paga.
-      </div>
-      <Divider
-        style={{ backgroundColor: "whitesmoke" }}
-        flexItem={true}
-        orientation="vertical"
-        variant="middle"
-      />
-      <div className={classes.skills}>
-        <ul>
+    <Grid
+      container
+      justify="space-between"
+      direction="row"
+      className={classes.skillBox}
+    >
+      <Grid item xs={12} md={5}>
+        <p style={{ textAlign: "justify" }}>
+          Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi.
+          Mauris aliquet nunc non turpis scelerisque, eget. Atirei o pau no
+          gatis, per gatis num morreus. Paisis, filhis, espiritis santis.
+          Posuere libero varius.
+          <br />
+          <br />
+          Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Aenean
+          aliquam molestie leo, vitae iaculis nisl. Per aumento de cachacis, eu
+          reclamis. Leite de capivaris, leite de mula manquis sem cabeça.
+          Manduma pindureta quium dia nois paga.
+        </p>
+      </Grid>
+      <Grid item xs="auto">
+        <Hidden mdUp lgUp xlUp>
+          <Divider
+            style={{ backgroundColor: "whitesmoke" }}
+            orientation="vertical"
+            variant="middle"
+          />
+        </Hidden>
+        <Hidden xsUp smUp>
+          <Divider
+            style={{ backgroundColor: "whitesmoke" }}
+            orientation="horizontal"
+            variant="middle"
+          />
+        </Hidden>
+      </Grid>
+      <Grid item alignItems="center" xs={12} md={5}>
+        <ul style={{ textAlign: "justify" }}>
           <li>Mussum Ipsum, cacilds vidis litro abertis.</li>
           <li>Praesent vel viverra nisi.</li>
           <li>Mauris aliquet nunc non turpis scelerisque, eget.</li>
@@ -59,8 +68,8 @@ const Skill: React.FC<{}> = () => {
             Posuere libero varius. Nullam a nisl ut ante blandit hendrerit.
           </li>
         </ul>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 };
 
